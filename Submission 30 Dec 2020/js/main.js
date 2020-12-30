36127da36fd5e2a315c49fcdbe20ac6d93eecf9c
LaMarsa <64203848+LaMarsa@users.noreply.github.com>
2020-12-30 19:09:57 +0000

    Add files via upload

---
+++ Submission 30 Dec 2020/js/main.js
@@ -0,0 +1,35 @@
+
+
+var bleep = new Audio();
+bleep.src = "sound/bleep.mp3"
+
+
+const navSlide = () => { 
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+
+
+burger.addEventListener('click', () => {
+	
+	nav.classList.toggle('nav-active');
+	
+	
+	navLinks.forEach((link, index) => {
+			
+			link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+				if (link.style.animation) {
+					link.style.animation = '';
+				} else {
+						link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';	
+				}
+		});
+		
+		burger.classList.toggle('toggle');
+
+	});
+}
+
+
+navSlide();
